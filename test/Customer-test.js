0739d56548f8e867f251406ae4fcf8affbea9b88
Shauna Myers <shaunamyers26@gmail.com>
2021-06-09 20:24:45 -0700

    Add sample data files for tests

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -13,6 +13,10 @@ describe('Customer Class', () => {
 
     it('Should be a function', () => {
         expect(Customer).to.be.a('function');
-    })
+    });
+
+    it('Should be an instance of Customer', () => {
+        expect(customer).to.be.an.instanceOf(Customer);
+    });
 
 })
