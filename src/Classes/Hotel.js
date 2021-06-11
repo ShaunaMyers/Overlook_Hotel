5c071e5ab63199ae6daa38370411a89bd7bf75d1
Shauna Myers <shaunamyers26@gmail.com>
2021-06-10 22:33:29 -0700

    Add files that were deleted prior and bring api data into scripts

---
+++ src/Classes/Hotel.js
@@ -0,0 +1,99 @@
+class Hotel {
+    constructor(rooms, bookings, guests) {
+        this.allRooms = rooms;
+        this.allReservations = bookings;
+        this.allGuests = guests;
+        this.roomsAvailable = [];
+        this.completedReservations = [];
+        this.currentReservations = [];
+        this.upcomingReservations = [];
+        this.totalRevenue = 0;
+    }
+
+
+    addToUpcomingReservations(booking) {
+        this.upcomingReservations.push(booking);
+    }
+
+    addToCurrentReservations(booking) {
+        this.currentReservations.push(booking);
+        if (this.upcomingReservations.includes(booking)) {
+            let bookingIndex = this.upcomingReservations.indexOf(booking);
+            this.upcomingReservations.splice(bookingIndex, 1);
+        }
+    }
+
+    addToCompletedReservations(booking) {
+        this.completedReservations.push(booking);
+        if (this.currentReservations.includes(booking)) {
+            let bookingIndex = this.currentReservations.indexOf(booking);
+            this.currentReservations.splice(bookingIndex, 1);
+        }
+    }
+
+    checkIfRoomsAreAvailable(customerSearch) {
+
+        let { date, roomType, numBeds, bedSize, costPerNight } = customerSearch;
+
+        let availableRoomNumbers = this.allReservations.filter(reservation => reservation.date !== date).map(room => room.roomNumber);
+
+        this.allRooms.forEach(room => {
+            if (availableRoomNumbers.includes(room.number)) {
+                this.roomsAvailable.push(room);
+            }
+        })
+
+        if (roomType || numBeds || bedSize || costPerNight) {
+            this.checkAvailabilityWithAllOptions(roomType, numBeds, bedSize, costPerNight);
+        }
+
+
+        if (this.roomsAvailable.length) {
+            return this.roomsAvailable;
+        } else {
+            return this.offerApologyMessage();
+        }
+    }
+
+    checkAvailabilityWithAllOptions(roomType, numBeds, bedSize, costPerNight) {
+        if (roomType) {
+            this.filterByRoomType(roomType);
+        }
+        if (numBeds) {
+            this.filterByNumberOfBeds(numBeds);
+        }
+        if (bedSize) {
+            this.filterByBedSize(bedSize);
+        }
+        if (costPerNight) {
+            this.filterByCostPerNight(costPerNight)
+        }
+    }
+
+
+    filterByRoomType(roomType) {
+        let availableRooms = this.roomsAvailable.filter(room => room.roomType === roomType);
+        this.roomsAvailable = availableRooms;
+    }
+
+    filterByNumberOfBeds(numBeds) {
+        let availableRooms = this.roomsAvailable = this.roomsAvailable.filter(room => room.numBeds === numBeds);
+        this.roomsAvailable = availableRooms;
+    }
+
+    filterByBedSize(bedSize) {
+        let availableRooms = this.roomsAvailable = this.roomsAvailable.filter(room => room.bedSize === bedSize);
+        this.roomsAvailable = availableRooms;
+    }
+
+    filterByCostPerNight(costPerNight) {
+        let availableRooms = this.roomsAvailable = this.roomsAvailable.filter(room => room.costPerNight <= costPerNight)
+        this.roomsAvailable = availableRooms;
+    }
+
+    offerApologyMessage() {
+        return 'We are very sorry...it looks like we have no rooms available that fit your search selections. We would love to host you, though, so please try your search using different selections.'
+    }
+}
+
+export default Hotel;
--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,14 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import './apiCalls';
+import apiCalls from './apiCalls';
+import Customer from './Classes/Customer';
+import Hotel from './Classes/Hotel';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-let customer, hotel, bookings, rooms;
+let customer, hotel;
 
 
 window.onload = onStartUp()
@@ -16,11 +18,11 @@ window.onload = onStartUp()
 function onStartUp() {
     apiCalls.getData()
         .then((promise) => {
+            customer = new Customer(promise[0].customers[(Math.floor(Math.random() * promise[0].customers.length) + 1)]);
+            console.log("CUSTOMER", customer);
 
-            customer = new Customer(promise[0][(Math.floor(Math.random() * promise[0].length) + 1)]);
-            ingredientsData = promise[1];
-            cookbook = new Cookbook(promise[2], promise[1]);
-            pantryInfo = new Pantry(user.pantry)
-            generateAllInfo();
+            hotel = new Hotel(promise[2].rooms, promise[1].bookings, promise[0].customers);
+            // generateAllInfo();
+            console.log("HOTEL", hotel);
         })
 }
