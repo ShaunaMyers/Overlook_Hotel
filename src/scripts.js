3099bbf14231ec20510e84e00b3bd5dac1231cf6
Shauna Myers <shaunamyers26@gmail.com>
2021-06-12 12:36:23 -0700

    Add first structure to HTML with tags and images

--- src/scripts.js
+++ src/scripts.js
@@ -3,13 +3,14 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import dayjs from 'dayjs';
 import apiCalls from './apiCalls';
 import Customer from './Classes/Customer';
 import Hotel from './Classes/Hotel';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import './images/mouldy-mango-resort.jpg'
 let customer, hotel;
 
 
@@ -19,10 +20,7 @@ function onStartUp() {
     apiCalls.getData()
         .then((promise) => {
             customer = new Customer(promise[0].customers[(Math.floor(Math.random() * promise[0].customers.length) + 1)]);
-            console.log("CUSTOMER", customer);
-
             hotel = new Hotel(promise[2].rooms, promise[1].bookings, promise[0].customers);
             // generateAllInfo();
-            console.log("HOTEL", hotel);
         })
 }
