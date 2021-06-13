a70c998602d1ce13a3f2b315765d18842135af55
Shauna Myers <shaunamyers26@gmail.com>
2021-06-12 21:33:36 -0700

    Add event listener for input with calendar

--- src/scripts.js
+++ src/scripts.js
@@ -15,8 +15,16 @@ import './images/queen-room2.jpg';
 import './images/right-arrow.svg';
 import './images/left-arrow.svg';
 
+// variables
+let searchByDateField = document.querySelector('#searchByDate');
 let customer, hotel;
 
+// event listeners
+searchByDateField.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        evaluateDateChosen(searchByDate.value);
+    }
+})
 
 window.onload = onStartUp()
 
@@ -28,3 +36,11 @@ function onStartUp() {
             // generateAllInfo();
         })
 };
+
+function evaluateDateChosen(value) {
+    console.log(value);
+    let searchDate = new Date(value);
+    console.log(searchDate);
+    console.log(Date.day());
+    // api date format "2020/01/24",
+}
