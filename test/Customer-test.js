5ab44463e2f25ab5bc39fd89cd506f1fb6e2d11f
Shauna Myers <shaunamyers26@gmail.com>
2021-06-15 16:28:02 -0700

    Fix should return all bookings test

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,17 +1,19 @@
 import { expect } from 'chai';
 import Customer from '../src/Classes/Customer';
+import Hotel from '../src/Classes/Hotel';
 import sampleCustomerData from '../src/data/sampleCustomerData';
 import sampleBookingsData from '../src/data/sampleBookingsData';
 import sampleRoomsData from '../src/data/sampleRoomsData';
 
 describe('Customer Class', () => {
 
-    let customer1, customer2;
+    let customer1, customer2, hotel;
 
     beforeEach(() => {
 
         customer1 = new Customer(sampleCustomerData[0]);
-        customer2 = new Customer(sampleCustomerData[2])
+        customer2 = new Customer(sampleCustomerData[2]);
+        hotel = new Hotel(sampleRoomsData, sampleBookingsData, sampleCustomerData);
 
     });
 
@@ -53,14 +55,30 @@ describe('Customer Class', () => {
         expect(customer2.completedBookings).to.eql([]);
     });
 
-    it('Should return all bookings from the past, present, and future', () => {
-        customer1.addToCompletedBookings(sampleBookingsData[0], sampleRoomsData[4]);
-        customer1.addToCurrentBookings(sampleBookingsData[2]);
-        customer1.addToUpcomingBookings(sampleBookingsData[3]);
+    it.only('Should return all bookings from the past, present, and future', () => {
+        const allBookings = customer1.returnAllBookings(hotel);
 
-        const allBookings = customer1.returnAllBookings();
-
-        expect(allBookings).to.eql({ completedBookings: [sampleBookingsData[0]], currentBookings: [sampleBookingsData[2]], upcomingBookings: [sampleBookingsData[3]] })
+        expect(allBookings).to.eql({
+  completedBookings: [
+    {
+      id: '5fwrgu4i7k55hl6t8',
+      userID: 1,
+      date: '2020/02/05',
+      roomNumber: 12,
+      roomServiceCharges: []
+    }
+  ],
+  currentBookings: [],
+  upcomingBookings: [
+    {
+      id: '5fwrgu4i7k55hl6t5',
+      userID: 1,
+      date: '2021/12/31',
+      roomNumber: 24,
+      roomServiceCharges: []
+    }
+  ]
+})
     });
 
     it('Should return an empty array when the Customer has no bookings in that specific property', () => {
